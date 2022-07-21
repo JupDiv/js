07c3066bc22683a9c8f1c369e41c845999522660
JupDev <97832488+JupDiv@users.noreply.github.com>
2022-07-21 21:43:13 +0300

    fix

--- Lesson_33/task_by_lesson/task_1/index.js
+++ Lesson_33/task_by_lesson/task_1/index.js
@@ -2,23 +2,28 @@ const userAvatar = document.querySelector('.user__avatar');
 const userName = document.querySelector('.user__name');
 const userLocation = document.querySelector('.user__location');
 
+const defaultAvatar = 'https://avatars.githubusercontent.com/u/1342004?v=4';
+userAvatar.src = defaultAvatar;
+const defaultUserName = 'Google';
+userName.textContent = defaultUserName;
+
 const sendRequestFetch = (name) => {
   return fetch(`http://api.github.com/users/${name}`).then((response) =>
     response.json()
   );
 };
-const userRender = (gitResponse) => {
-  const { avatar_url, name, location } = gitResponse;
+const userRender = (userData) => {
+  const { name, avatar_url, location } = userData;
   userAvatar.src = avatar_url;
   userName.textContent = name;
   userLocation.textContent = location ? `from ${location}` : null;
 };
-const userNameInput = document.querySelector('.name-form__input');
-const userBtnInput = document.querySelector('.name-form__btn');
+const inputData = document.querySelector('.name-form__input');
+const btnInput = document.querySelector('.name-form__btn');
 
 const onClickButton = () => {
-  const getName = userNameInput.value;
+  const getName = inputData.value;
   sendRequestFetch(getName).then((response) => userRender(response));
 };
 
-userBtnInput.addEventListener('click', onClickButton);
+btnInput.addEventListener('click', onClickButton);
