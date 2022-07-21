d0c3584026a9a6270d9499d937dda153af943dcb
JupDev <97832488+JupDiv@users.noreply.github.com>
2022-07-21 22:09:46 +0300

    fix

--- Lesson_33/task_by_home/task_1/index.js
+++ Lesson_33/task_by_home/task_1/index.js
@@ -1,18 +1,20 @@
 const baseUrl = 'https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/tasks';
 
-function getTasksList() {
-  // put your code here
+export function getTasksList() {
+  return fetch(baseUrl).then((response) => response.json());
 }
 
-function getTaskById(taskId) {
-  // put your code here
+export function getTaskById(taskId) {
+  return fetch(baseUrl)
+    .then((response) => response.json())
+    .then((data) => data.filter((elem) => taskId === elem.id));
 }
 
 // examples
-getTasksList().then((tasksList) => {
-  console.log(tasksList); // ==> [ {'id':'1', 'isDone':false ... }, {'id':'2', 'isDone':false ... }, ...]
-});
+// getTasksList().then((tasksList) => {
+//   console.log(tasksList); // ==> [ {'id':'1', 'isDone':false ... }, {'id':'2', 'isDone':false ... }, ...]
+// });
 
-getTaskById('2').then((taskData) => {
-  console.log(taskData); // ==> { 'id': '2', 'text': 'District Communications Specialist', 'isDone': false, 'createdDate': 1651499052, 'finishedDate': 1651499052 }
-});
+// getTaskById('1').then((taskData) => {
+//   console.log(taskData); // ==> { 'id': '2', 'text': 'District Communications Specialist', 'isDone': false, 'createdDate': 1651499052, 'finishedDate': 1651499052 }
+// });
